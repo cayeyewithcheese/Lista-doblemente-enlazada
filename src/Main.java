f539da9b8d5e1ed99690f2309a2a88ed78424361
mel <meroqui31@gmail.com>
2026-09-04 10:50:34 -0500

    Booleano esta vacio

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        MiListaDoble lista = new MiListaDoble();
+
+        System.out.println("Lista doble");
+    }
+}
---
+++ src/MiListaDoble.java
@@ -0,0 +1,102 @@
+public class MiListaDoble implements ListInterface{
+
+    private DoubleNode head;
+    private DoubleNode tail;
+    private int size;
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object getHead() {
+        return null;
+    }
+
+    @Override
+    public Object getTail() {
+        return null;
+    }
+
+    @Override
+    public Object get(DoubleNode node) {
+        return null;
+    }
+
+    @Override
+    public DoubleNode search(Object object) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean insert(DoubleNode node, Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean insert(Object objectRef, Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean insertHead(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean insertTail(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean set(DoubleNode node, Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(DoubleNode node) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] object) {
+        return new Object[0];
+    }
+
+    @Override
+    public MiListaDoble subList(DoubleNode from, DoubleNode to) {
+        return null;
+    }
+
+    @Override
+    public MiListaDoble sortList() {
+        return null;
+    }
+
+}
