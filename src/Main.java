e3bcf70af465a67ebc4feeb0d7c89ff2f4e53e94
mel <meroqui31@gmail.com>
2026-09-04 11:04:53 -0500

    Obteniendo el tamaño correccion

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class Main {
     public static void main(String[] args){
         MiListaDoble lista = new MiListaDoble();
 
-        System.out.println("Esta vacia?: " + lista.isEmpty());
+        System.out.println("ADD ");
+        lista.add(10.0);
+        lista.add(20.0);
+        lista.add(30.0);
+        lista.add(40.0);
+        System.out.println("Elementos agregados.");
+        System.out.println("Tamaño: " + lista.getSize());
+
+
     }
 }
